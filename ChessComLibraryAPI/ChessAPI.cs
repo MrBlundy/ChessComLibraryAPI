dac37b8e51aae45f6ccc3ed4ca0a1873c4fafc59
Dustin Greene <dustingreeneatx512@gmail.com>
2021-04-06 11:24:44 -0500

    Added endpoints for Tournaments,Clubs,Country,Puzzles

--- ChessComLibraryAPI/ChessAPI.cs
+++ ChessComLibraryAPI/ChessAPI.cs
@@ -1,7 +1,10 @@
 ﻿using ChessComLibraryAPI.Models.Clubs;
+using ChessComLibraryAPI.Models.Countries;
 using ChessComLibraryAPI.Models.Games;
 using ChessComLibraryAPI.Models.Misc;
 using ChessComLibraryAPI.Models.Stats;
+using ChessComLibraryAPI.Models.TeamMatches;
+using ChessComLibraryAPI.Models.Tournaments;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -191,10 +194,10 @@ namespace ChessComLibraryAPI
         }
 
         // List of Team matches the player has attended, is participating or is currently registered.
-        public static async Task<TeamMatches> GetTeamMatchesAsync(string username)
+        public static async Task<Models.Participation.TeamMatches> GetTeamMatchesAsync(string username)
         {
             var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/player/{username}/matches").ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<TeamMatches>(json);
+            return JsonConvert.DeserializeObject<Models.Participation.TeamMatches>(json);
         }
 
         #endregion
@@ -216,30 +219,164 @@ namespace ChessComLibraryAPI
         }
 
         // List of daily and club matches, grouped by status (registered, in progress, finished).
-        public static async Task<ClubMembers> GetClubMatchesAsync(string url_id)
+        public static async Task<ClubMatches> GetClubMatchesAsync(string url_id)
         {
             var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/club/{url_id}/matches").ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<ClubMembers>(json);
+            return JsonConvert.DeserializeObject<ClubMatches>(json);
         }
 
         #endregion
 
-
         #region Tournaments
+
+        /// <summary>
+        /// Get details about a daily, live and arena tournament
+        /// </summary>
+        /// <param name="url_id"></param>
+        /// <returns></returns>
+        public static async Task<Tournament> GetTournamentAsync(string url_id)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/tournament/{url_id}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<Tournament>(json);
+        }
+
+        /// <summary>
+        /// Get details about a tournament's round
+        /// </summary>
+        /// <param name="url_id"></param>
+        /// <returns></returns>
+        public static async Task<TournamentRound> GetTournamentRoundAsync(string url_id, string round)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/tournament/{url_id}/{round}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<TournamentRound>(json);
+        }
+
+        /// <summary>
+        /// Get details about a tournament's round group
+        /// </summary>
+        /// <param name="url_id"></param>
+        /// <param name="round"></param>
+        /// <returns></returns>
+        public static async Task<TournamentRoundGroup> GetTournamentRoundGroupAsync(string url_id, string round, string group)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/tournament/{url_id}/{round}/{group}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<TournamentRoundGroup>(json);
+        }
         #endregion
 
         #region Team Matches
+
+        /// <summary>
+        /// Get details about a team match and players playing that match.
+        /// After the match is finished there will be a link to each player's stats endpoint,
+        /// in order to get up-to-date information about the player.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static async Task<Models.TeamMatches.TeamMatch> GetDailyTeamMatchAsync(string id)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/match/{id}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<Models.TeamMatches.TeamMatch>(json);
+        }
+        
+        /// <summary>
+        /// Get details about a team match board.
+        /// Only in-progress or finished games will be included,
+        /// so there may be one or two games in this list
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="board"></param>
+        /// <returns></returns>
+        public static async Task<TeamMatchBoard> GetDailyTeamMatchBoardAsync(string id, string board)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/match/{id}/{board}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<TeamMatchBoard>(json);
+        }
+
+        /// <summary>
+        /// Get details about a team match and players playing that match.
+        /// After the match is finished there will be a link to each player's stats endpoint,
+        /// in order to get up-to-date information about the player.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static async Task<TeamMatch> GetLiveTeamMatchAsync(string id)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/match/live/{id}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<TeamMatch>(json);
+        }
+
+        /// <summary>
+        /// Get details about a team match board.
+        /// Only in-progress or finished games will be included,
+        /// so there may be one or two games in this list
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="board"></param>
+        /// <returns></returns>
+        public static async Task<TeamMatchBoard> GetLiveTeamMatchBoardAsync(string id, string board)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/match/live/{id}/{board}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<TeamMatchBoard>(json);
+        }
+
         #endregion
 
         #region Countries
+
+        /// <summary>
+        /// Get additional details about a country
+        /// </summary>
+        /// <param name="iso"></param>
+        /// <returns></returns>
+        public static async Task<CountryProfile> GetCountryProfileAsync(string iso)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/country/{iso}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<CountryProfile>(json);
+        }
+
+        /// <summary>
+        /// List of usernames for players who identify themselves as being in this country
+        /// </summary>
+        /// <param name="iso"></param>
+        /// <returns></returns>
+        public static async Task<string[]> GetPlayersFromCountryAsync(string iso)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/country/{iso}/players").ConfigureAwait(false);
+            return JObject.Parse(json)["players"].ToObject<string[]>();
+        }
+
+        /// <summary>
+        /// List of URLs for clubs identified as being in or associated with this country
+        /// </summary>
+        /// <param name="iso"></param>
+        /// <returns></returns>
+        public static async Task<string[]> GetClubsFromCountryAsync(string iso)
+        {
+            var json = await GetJsonFromUrlAsync($"https://api.chess.com/pub/country/{iso}/clubs").ConfigureAwait(false);
+            return JObject.Parse(json)["players"].ToObject<string[]>();
+        }
         #endregion
 
         #region Daily Puzzle
 
-        // retrieve the daily puzzle
+        /// <summary>
+        /// Information about the daily puzzle found in www.chess.com
+        /// </summary>
+        /// <returns>DailyPuzzle</returns>
         public static async Task<DailyPuzzle> GetDailyPuzzleAsync()
         {
             var json = await GetJsonFromUrlAsync("https://api.chess.com/pub/puzzle").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<DailyPuzzle>(json);
+        }
+
+        /// <summary>
+        /// Information about a randomly picked daily puzzle.
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<DailyPuzzle> GetRandomPuzzleAsync()
+        {
+            var json = await GetJsonFromUrlAsync("https://api.chess.com/pub/puzzle/random").ConfigureAwait(false);
             return JsonConvert.DeserializeObject<DailyPuzzle>(json);
         }
 
