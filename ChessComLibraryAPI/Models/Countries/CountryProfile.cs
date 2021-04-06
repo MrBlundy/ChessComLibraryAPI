d131adb9239c38b659a6dfc629083b66e26f0015
Dustin Greene <dustingreeneatx512@gmail.com>
2021-04-06 11:20:15 -0500

    Added Model for Countries

---
+++ ChessComLibraryAPI/Models/Countries/CountryProfile.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace ChessComLibraryAPI.Models.Countries
+{
+    /// <summary>
+    /// Get additional details about a country
+    /// </summary>
+    public class CountryProfile
+    {
+        /// <summary>
+        /// The location of this profile (always self-referencing)
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// The human-readable name of this country
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The ISO-3166-1 2-character code
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
